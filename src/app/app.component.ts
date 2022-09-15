3f8d8853e7b12e734d465ebe2ec1e51eae8025dc
Dipak sisodiya <dipaksisodiya726@gmail.com>
2022-09-15 12:35:12 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,7 @@ import { ConfirmPasswordValidator } from "./confirm-password.validatiors";
 })
 export class AppComponent {
   title = 'gitHub-project';
-  submitted=false
-  public showPassword: boolean;
-  public showCPassword: boolean;
-  
-  signUpForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
-    this.signUpForm = this.formBuilder.group({
-        password: ["", [Validators.required, Validators.minLength(6)]],
-        confirmPassword: ["", Validators.required]
-      },
-      {
-        validator: ConfirmPasswordValidator("password", "confirmPassword")
-      }
-    );
-  }
-   submit(data){
 
-   console.log(data)
-   }
-  ngOnInit(): void {
-  }
 
 
 
