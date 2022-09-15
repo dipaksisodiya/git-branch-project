85146eec51ceb3716f1d23a4799d571e7e3fc37f
Dipak sisodiya <dipaksisodiya726@gmail.com>
2022-09-15 15:49:56 +0530

    auto-complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, } from 'rxjs';
+import {map, startWith} from 'rxjs/operators'
+
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'gitHub-project';
-  constructor(){
-    console.log("ddd")
-    this.data()
-  }
 
-  data(){
+  options : string[]=['Angular','JavaScript','Java','TypeScript']
+ 
+  selectedOptions = this.options;
 
-  }
+  inputdata(value){
+    this.selectedOptions = this.search(value);
+   
+    
+   
+} 
+search(value:string){
+
+  let filter = value.toLowerCase();
+  return this.options.filter(option =>
+   option.toLowerCase().startsWith(filter)
+    );
+   
 }
+ 
+
+
+}    
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
